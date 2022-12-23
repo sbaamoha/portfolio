c316242aefb79bae3b05e9398d85624e79ae6d41
sbaamoha <sbaamohe@gmail.com>
2022-12-23 19:54:22 +0100

    added ecommerce project

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -2,55 +2,64 @@ import { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { projects } from "../public/constants";
-import {CgMediaLive} from "react-icons/cg"
-import {AiFillGithub} from "react-icons/ai"
+import { CgMediaLive } from "react-icons/cg";
+import { AiFillGithub } from "react-icons/ai";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Portfolio: React.FC = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-    useEffect(() => {
-        AOS.init()
-      },[])
-
-    return (
-        <div id="projects">
-            <h2 className="uppercase text-4xl text-center">my <span className="text-secondary-color">projects</span></h2>
-            <div className="py-12 grid grid-cols-1 lg:grid-cols-3 gap-6 px-6 lg:px-12">
-                {projects.map(project => 
-                    <div
-                        className="rounded-lg shadow-lg"
-                        key={project.id}>
-                        <Link href={project.live}>
-                            <Image 
-                                data-aos="fade-right"
-                                data-aos-delay="100" 
-                                data-aos-duration="1000"
-                                className="rounded-lg cursor-pointer" 
-                                src={project.img} alt={project.title} 
-                                width={500} 
-                                height={50} 
-                                objectFit='cover' /></Link>
-                        <div className="p-4">
-                            <Link href={project.live}>
-                                <h2 className="uppercase cursor-pointer text-2xl">{project.title} </h2>
-                            </Link>
-                            <p className="capitalize">{project.desc} </p>
-                            <div className="flex gap-2 uppercase pt-6">
-                                <Link className="flex items-center gap-2 p-2 shadow-lg hover:bg-secondary-color hover:text-white transition-all rounded-full" href={project.live}>
-                                    <CgMediaLive/> live
-                                </Link>
-                                <Link className="flex items-center gap-2 p-2 shadow-lg hover:bg-secondary-color hover:text-white transition-all rounded-full" href={project.github}>
-                                     <AiFillGithub/> github
-                                </Link>
-                        </div>
-                    </div>
-                </div>
-                )}
+  return (
+    <div id="projects">
+      <h2 className="uppercase text-4xl text-center">
+        my <span className="text-secondary-color">projects</span>
+      </h2>
+      <div className="py-12 grid grid-cols-1 lg:grid-cols-3 gap-6 px-6 lg:px-12">
+        {projects.map((project) => (
+          <div className="rounded-lg shadow-lg" key={project.id}>
+            <Link href={project.live}>
+              <Image
+                data-aos="fade-right"
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                className="rounded-lg cursor-pointer"
+                src={project.img}
+                alt={project.title}
+                width={500}
+                height={50}
+              />
+            </Link>
+            <div className="p-4">
+              <Link href={project.live}>
+                <h2 className="uppercase cursor-pointer text-2xl">
+                  {project.title}{" "}
+                </h2>
+              </Link>
+              <p className="capitalize">{project.desc} </p>
+              <div className="flex gap-2 uppercase pt-6">
+                <Link
+                  className="flex items-center gap-2 p-2 shadow-lg hover:bg-secondary-color hover:text-white transition-all rounded-full"
+                  href={project.live}
+                >
+                  <CgMediaLive /> live
+                </Link>
+                <Link
+                  className="flex items-center gap-2 p-2 shadow-lg hover:bg-secondary-color hover:text-white transition-all rounded-full"
+                  href={project.github}
+                >
+                  <AiFillGithub /> github
+                </Link>
+              </div>
             </div>
-        </div>
-    );
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- public/constants/projects.ts
+++ public/constants/projects.ts
@@ -1,28 +1,35 @@
-import {images} from './index';
-export const projects =  [
-    {
-        id: 1,
-        title: 'hoobank landing page',
-        desc: 'responsive landing page',
-        img: images.hoobank,
-        github: 'https://github.com/sbaamoha/hoobank-landingpage',
-        live: 'https://hoobank-landingpage.vercel.app'
-    },
-    {
-        id: 2,
-        title: 'next generation landing page',
-        desc: 'landing page',
-        img: images.nextGeneration,
-        github: 'https://github.com/sbaamoha/project-003',
-        live: 'https://sbaamoha.github.io/project-003/'  
-    },
-    {
-        id: 3,
-        title: 'bookmark landing page',
-        desc: 'mobile friendly landing page',
-        img: images.bookmark,
-        github: 'https://github.com/sbaamoha/frontend001/tree/master',
-        live: 'https://sbaamoha.github.io/frontend001/src/'  
-    },
-    
-]
+import { images } from "./index";
+export const projects = [
+  {
+    id: 1,
+    title: "hoobank landing page",
+    desc: "responsive landing page",
+    img: images.hoobank,
+    github: "https://github.com/sbaamoha/hoobank-landingpage",
+    live: "https://hoobank-landingpage.vercel.app",
+  },
+  {
+    id: 2,
+    title: "next generation landing page",
+    desc: "landing page",
+    img: images.nextGeneration,
+    github: "https://github.com/sbaamoha/project-003",
+    live: "https://sbaamoha.github.io/project-003/",
+  },
+  {
+    id: 3,
+    title: "bookmark landing page",
+    desc: "mobile friendly landing page",
+    img: images.bookmark,
+    github: "https://github.com/sbaamoha/frontend001/tree/master",
+    live: "https://sbaamoha.github.io/frontend001/src/",
+  },
+  {
+    id: 4,
+    title: "sba e-commerce",
+    desc: "MERN stack ecommerce website with authentication and authorization with JWT",
+    img: images.ecommerce,
+    github: "https://github.com/sbaamoha/ecommerce-front",
+    live: "https://snazzy-sundae-0762e9.netlify.app/",
+  },
+];
