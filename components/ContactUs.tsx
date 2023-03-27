65b36e06b9c53083eb85b2e650bb5eaa453efac0
sbaamoha <sbaamohe@gmail.com>
2023-03-27 18:46:38 +0100

    fix duplicated email

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,16 +1,15 @@
-import Link from "next/link";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
 import * as emailjs from "emailjs-com";
 import { useState } from "react";
+import { toast } from "react-toastify";
 const ContactUs = () => {
-  const [name, setName] = useState<string>();
-  const [email, setEmail] = useState<string>();
-  const [message, setMessage] = useState<string>();
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if(!name || !email || !message){
-      return 'fill the data'
-      }
+    e.preventDefault();
+    if (!name || !email || !message) {
+      return "fill the data";
+    }
     const template_params = {
       from_email: email,
       from_name: name,
@@ -19,40 +18,26 @@ const ContactUs = () => {
     };
     const service_id = "service_z9e5g9f";
     const template_id = "template_qpqxue4";
-    emailjs
+    return emailjs
       .send(service_id, template_id, template_params, "eKMTuVxoTrFA8pNI9")
-      .then((res) =>   
-           
-           
-           
-   console.log("success")
-
-            
-    
-
-)
- 
-
-
+      .then(() => {
+        setName("");
+        setEmail("");
+        setMessage("");
+        window.scrollTo(0, 0);
+        // toast.success("Email send successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("can't send email");
+      });
   };
   return (
-    <div className="md:flex flex-col md:justify-between gap-6 p-12 capitalize rounded-md shadow-md">
+    <div className="w-[50%] m-auto my-6 md:flex flex-col md:justify-between gap-6 p-12 capitalize rounded-md shadow-md">
       <h2 className="uppercase text-4xl text-center pb-12">
         contact <p className="text-secondary-color inline">me</p>
       </h2>
-      {/* <div className="hidden md:flex flex-1 bg-secondary-color text-white p-6 rounded-sm">
-        <h2 className="text-2xl text-center md:text-start mb-6">about me</h2>
-        <p className="my-2">phone : +213664512005</p>
-        <p>country : Algeria</p>
-        <div className="flex justify-end mt-6 gap-2 text-lg ">
-          <Link href={`${process.env.NEXT_PUBLIC_GITHUB_URL}`}>
-            <BsGithub />
-          </Link>
-          <Link href={`${process.env.NEXT_PUBLIC_LINKEDIN_URL}`}>
-            <BsLinkedin />
-          </Link>
-        </div>
-      </div> */}
+
       <form
         onSubmit={handleSubmit}
         className="flex flex-1 justify-start flex-col gap-1"
@@ -60,30 +45,38 @@ const ContactUs = () => {
         <label>name</label>
         <input
           type="text"
-          name="name"
+          value={name}
           placeholder="Enter Your name"
           className="rounded-md p-2"
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <label>email</label>
         <input
           type="email"
-          name="email"
+          value={email}
           placeholder="Enter Your email"
           className="rounded-md p-2"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <label>message</label>
         <textarea
           placeholder="Enter Your message"
           className="rounded-md p-2"
-          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
         />
         <button
           type="submit"
+          disabled={name.length === 0 || email.length === 0}
           className="rounded-sm bg-secondary-color text-white capitalize py-2 mt-3"
         >
-          submit
+          send email
         </button>
       </form>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { ToastContainer } from "react-toastify";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <ToastContainer />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
