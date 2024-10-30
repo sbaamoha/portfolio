f14635590ed7c07e0709d29109289e8e217009eb
Mohamed Sba <sbaamohe@gmail.com>
2024-10-30 12:21:03 +0100

    update: add dark mode in toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,36 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CursorTrailCanvas from "../components/CursorTrailCanvas";
+const ToastWrapper = () => {
+  const { theme } = useTheme();
 
+  return (
+    <ToastContainer
+      position="top-right"
+      // autoClose={5000}
+      hideProgressBar={true}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme={theme === "dark" ? "dark" : "light"} // Use theme from context
+      // className={`${
+      //   theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"
+      // }`}
+      toastClassName={`${
+        theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"
+      } border rounded-lg p-4 shadow-md`}
+    />
+  );
+};
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <CursorTrailCanvas className="pointer-events-none fixed inset-0 z-10 h-full w-full" />
       <Component {...pageProps} />
-      <ToastContainer />
+      <ToastWrapper />
     </ThemeProvider>
   );
 }
