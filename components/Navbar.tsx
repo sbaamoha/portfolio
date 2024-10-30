126f48ac34dc74b1c225a98266245033c0b3b559
Mohamed Sba <sbaamohe@gmail.com>
2024-10-30 14:17:37 +0100

    fix: the sidebar collapse on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 // react icons
 import { BsGithub, BsLinkedin } from "react-icons/bs";
@@ -10,11 +10,32 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useTheme } from "next-themes";
 import { UseThemeProps } from "next-themes/dist/types";
+import { useIsMobile } from "../hooks/use-mobile";
 
 const Navbar = () => {
+  const isMobile = useIsMobile();
   const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
-
+  // use state snippet
+  const [navIsOpen, setnavIsOpen] = useState<boolean>(false);
+  const sidebarRef = useRef<HTMLUListElement | null>(null);
+  useEffect(() => {
+    const handleCloseSidebar = (event: MouseEvent) => {
+      if (
+        navIsOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setnavIsOpen(false);
+      }
+    };
+    if (isMobile && navIsOpen) {
+      window.addEventListener("mousedown", handleCloseSidebar);
+    }
+    return () => {
+      window.removeEventListener("mousedown", handleCloseSidebar);
+    };
+  }, [isMobile, navIsOpen]);
   const renderThemeChanger = () => {
     if (!mounted) return null;
     const currentTheme = theme === "system" ? systemTheme : theme;
@@ -35,8 +56,6 @@ const Navbar = () => {
     }
   };
 
-  // use state snippet
-  const [navIsOpen, setnavIsOpen] = useState<boolean>(true);
   // init animations
   useEffect(() => {
     AOS.init();
@@ -52,34 +71,38 @@ const Navbar = () => {
     >
       <Link
         href="/"
-        className="flex capitalize transition-all gap-2 items-center text-2xl"
+        className="flex capitalize transition-all gap-2 items-center text-2xl fl border-b rounded-sm px-2"
       >
         <h3>mohamed</h3>
       </Link>
-
+      <div className="flex-1" />
       <div
         className="humberger lg:hidden cursor-pointer "
         onClick={() => setnavIsOpen((prev) => !prev)}
       >
-        {navIsOpen ? <BiMenu /> : <BiMenuAltRight />}
+        {!navIsOpen ? <BiMenu /> : <BiMenuAltRight />}
       </div>
-      <ul className="ul">
+      <ul className="ul border rounded-full max-w-fit p-1">
         {["about", "experience", "projects", "contact"].map((item) => (
-          <li className="flex items-center" key={item}>
-            <div className="w-2 h-2 rounded-full" />
+          <li
+            className="flex items-center gap-2 p-2 rounded-full shadow-xl bg-blend-darken hover:scale-105 transition border"
+            key={item}
+          >
+            {/* <div className="w-2 h-2 rounded-full" /> */}
             <Link href={`#${item}`}>{item}</Link>
           </li>
         ))}
       </ul>
 
       <ul
-        className={`ulCol lg:hidden flex-col bg-primary-color dark:bg-slate-800 dark:text-white absolute top-[60px] shadow-lg w-[60vw] h-[90vh] p-6 ${
-          !navIsOpen ? `right-0` : `right-[100%]`
-        } `}
+        ref={sidebarRef}
+        className={`ulCol lg:hidden flex-col bg-primary-color transition-all duration-300 ease-in-out dark:bg-slate-800 dark:text-white absolute top-[60px] shadow-lg w-[60vw] h-[90vh] p-6 ${
+          navIsOpen ? `right-0 opacity-100` : `-right-full opacity-0`
+        }`}
       >
         {["projects", "technologies", "about"].map((item) => (
           <li
-            onClick={() => setnavIsOpen(true)}
+            onClick={() => setnavIsOpen(false)}
             className="btnCol my-6"
             key={item}
           >
